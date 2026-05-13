23bc42c63525f0103e198a32f5359c74c2025e0c
Nyan <phamthanhnhanussh@gmail.com>
2026-05-13 13:59:15 +0700

    added custom logging

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "Logger.h"
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
 #include <SDL2/SDL_image.h>
@@ -8,16 +9,16 @@
 
 Game::Game() {
     isRunning = false;
-    std::cout << "Game constructor called!\n";
+    Logger::Log("Game constructor called!");
 }
 
 Game::~Game() {
-    std::cout << "Game destructor called!\n";
+    Logger::Log("Game destructor called!");
 }
 
 void Game::Initialize() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        std::cerr << "Error initializing SDL.\n";
+        Logger::Error("Error initializing SDL.");
         return;
     }
     SDL_DisplayMode displayMode;
@@ -33,12 +34,12 @@ void Game::Initialize() {
         SDL_WINDOW_BORDERLESS
     );
     if (!window) {
-        std::cerr << "Error creating SDL window.\n";
+        Logger::Error("Error creating SDL window.");
         return;
     }
     renderer = SDL_CreateRenderer(window, -1, 0);
     if (!renderer) {
-        std::cerr << "Error creating SDL renderer.\n";
+        Logger::Error("Error creating SDL renderer.");
         return;
     }
     SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN); 
